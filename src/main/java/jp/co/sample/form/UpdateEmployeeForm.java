47f603fc392f3099f6c7592c643adf618da936e0
iwagane <asami.iwagane@rakus-partners.co.jp>
2022-02-09 18:02:21 +0900

    入社日更新途中まで

--- src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
+++ src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
@@ -1,15 +1,43 @@
 package jp.co.sample.form;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class UpdateEmployeeForm {
 	private String id;
+	private String name;
+	private String gender;
+	private String hireDate;
+	private String mailAddress;
+	private String zipCode;
+	private String address;
+	private String telephone;
+	private String salary;
+	private String characteristics;
 	private String dependentsCount;
-	
+
 	public int getIntId() {
 		return Integer.parseInt(id);
 	}
 
 	public int getIntDependentsCount() {
 		return Integer.parseInt(dependentsCount);
+	}
+
+	public int getIntSaraly() {
+		return Integer.parseInt(salary);
+	}
+
+	public Date getDateHireDate() {
+		SimpleDateFormat format = new SimpleDateFormat();
+		try {
+			return format.parse(hireDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public String getId() {
@@ -28,8 +56,84 @@ public class UpdateEmployeeForm {
 		this.dependentsCount = dependentsCount;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(String hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public String getMailAddress() {
+		return mailAddress;
+	}
+
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getSalary() {
+		return salary;
+	}
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+
+	public String getCharacteristics() {
+		return characteristics;
+	}
+
+	public void setCharacteristics(String characteristics) {
+		this.characteristics = characteristics;
+	}
+
 	@Override
 	public String toString() {
-		return "UpdateEmployeeForm [id=" + id + ", dependentsCount=" + dependentsCount + "]";
+		return "UpdateEmployeeForm [id=" + id + ", name=" + name + ", gender=" + gender + ", hireDate=" + hireDate
+				+ ", mailAddress=" + mailAddress + ", zipCode=" + zipCode + ", address=" + address + ", telephone="
+				+ telephone + ", salary=" + salary + ", characteristics=" + characteristics + ", dependentsCount="
+				+ dependentsCount + "]";
 	}
+
 }
